131482703c6eb18a7093810555af5d39cc9c06cc
omar reda <omaraladeeb45@gmail.com>
2025-04-11 23:53:16 +0200

    add a 2 features new arrival + best

---
+++ Services/FeatureService.cs
@@ -0,0 +1,27 @@
+﻿using IMDB.Models;
+using IMDB.MyContext;
+using IMDB.ViewModels;
+
+namespace IMDB.Services
+{
+    public class FeatureService : IFeatures
+    {
+        private readonly Applicationcontext context;
+
+        public FeatureService(Applicationcontext context)
+        {
+            this.context = context;
+        }
+
+        // will order by rating 
+        public List<Media> GetBest()
+        {
+             throw new NotImplementedException(); 
+        }
+
+        public List<Media> GetNewArrival()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IFeatures.cs
@@ -0,0 +1,12 @@
+﻿using IMDB.Models;
+using IMDB.ViewModels;
+
+namespace IMDB.Services
+{
+    public interface IFeatures
+    {
+        public List<Media> GetNewArrival();
+
+        public List<Media> GetBest(); 
+    }
+}
